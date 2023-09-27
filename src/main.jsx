9294328c4ebd22e8bec4d3796e73cc751287d25e
Jacob Carpenter <github@jacobcarpenter.com>
2023-09-27 08:37:20 -0700

    set up pages deploy?

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App.jsx";
 import { Test } from "./Test.jsx";
 
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "/test",
-		element: <Test />,
-	},
-]);
+const router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
+		},
+		{
+			path: "/test",
+			element: <Test />,
+		},
+	],
+	{ basename: "/ahlcg-tokens" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
